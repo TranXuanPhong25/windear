9f90bc807bf36f3de7dd1ef392685f51965647ca
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-08 08:56:17 +0700

    feat(ui): init shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {}
+    }
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
