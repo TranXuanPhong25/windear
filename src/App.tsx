a1eef256af8376b432c52aabeb56662eff68ea0a
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-14 23:33:29 +0700

    feat: add 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,20 @@ import RequireAuth from './components/auth/RequireAuth';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import HomePage from './pages/home/HomePage';
 import LogoutPage from './pages/auth/LogoutPage';
+import BookShow from './pages/books/BookShow';
+import NotFound from './pages/notfound/NotFound';
 export default function App() {
    return (
       <AuthProvider>
          <Routes>
             <Route>
-               <Route path="*" element={<div>not found</div>} />
+               <Route path="*" element={<NotFound/>} />
                <Route element={<Layout />} >
                   <Route path="/" element={<HomePage />} />
                   <Route path="/shelves" element={<div>shelves</div>} />
                   <Route path="/browse/*" element={<div>browse</div>} />
                   <Route path="/books/" element={<div>book</div>} /> 
-                  <Route path="/books/:id" element={<div>book with id</div>} />
+                  <Route path="/books/:bookId" element={<BookShow/>} />
                   <Route path="/user/notifications" element={<div>noti</div>} />
                   <Route path="/user/" element={<div>user</div>} />
                   <Route path="/user/:id" element={<div>user with id</div>} />
