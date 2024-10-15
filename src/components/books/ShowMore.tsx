76b30f965552b8fe038c939da56b72afd1ec5d26
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-15 21:46:29 +0700

    feat: add tag, description for BookShow

---
+++ src/components/books/ShowMore.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
+interface ExpandableParagraphProps {
+   text: string
+   maxLength?: number
+}
+
+export default function ExpandableParagraph({
+   text = '',
+   maxLength = 300
+}: ExpandableParagraphProps) {
+   const [isExpanded, setIsExpanded] = useState(false)
+
+   const toggleExpand = () => {
+      setIsExpanded(!isExpanded)
+   }
+
+   const shouldTruncate = text.length > maxLength
+   const displayText = isExpanded || !shouldTruncate ? text : `${text.slice(0, maxLength)}...`
+
+   return (
+      <div className=" py-3 text-white rounded-md text-justify text-md">
+         <p className={`${isExpanded ? '' : 'line-clamp-4'}`} >
+            {displayText}
+            {shouldTruncate && (
+               <Button
+                  variant="link"
+                  size="sm"
+                  className="text-purple-400 hover:text-purple-300 p-0 ml-3 text-md h-auto"
+                  onClick={toggleExpand}
+               >
+                  {isExpanded ? 'Hide' : 'Show more'}
+               </Button>
+            )}
+         </p>
+
+      </div>
+   )
+}
--- src/pages/books/BookShow.tsx
+++ src/pages/books/BookShow.tsx
@@ -5,9 +5,99 @@ import {
    PopoverContent,
    PopoverTrigger,
 } from "@/components/ui/popover"
-import { Button } from "@/components/ui/button";
 import { Close } from "@radix-ui/react-popover";
 import { BookCheckIcon, BookmarkIcon, BookOpenTextIcon } from "lucide-react";
+import ExpandableParagraph from "@/components/books/ShowMore";
+import { Link } from "react-router-dom";
+
+const genreTags = [
+   {
+      "slug": "fiction",
+      "tag_category": {
+         "category": "Genre"
+      },
+      "tag": "Fiction"
+   }
+   , {
+      "slug": "fantasy",
+      "tag_category": {
+         "category": "Genre"
+      },
+      "tag": "Fantasy"
+   },
+   {
+      "slug": "young-adult",
+      "tag_category": {
+         "category": "Genre"
+      },
+      "tag": "Young Adult"
+   },
+   {
+      "slug": "science-fiction",
+      "tag_category": {
+         "category": "Genre"
+      },
+      "tag": "Science fiction"
+   }
+]
+const moodTags = [
+   {
+      "slug": "lighthearted",
+      "tag_category": {
+         "category": "Mood"
+      },
+      "tag": "lighthearted"
+   },
+
+   {
+      "slug": "hopeful",
+      "tag_category": {
+         "category": "Mood"
+      },
+      "tag": "hopeful"
+   }
+   ,
+   {
+      "slug": "emotional",
+      "tag_category": {
+         "category": "Mood"
+      },
+      "tag": "emotional"
+   },
+   {
+      "slug": "reflective",
+      "tag_category": {
+         "category": "Mood"
+      },
+      "tag": "reflective"
+   },
+   {
+      "slug": "mysterious",
+      "tag_category": {
+         "category": "Mood"
+      },
+      "tag": "mysterious"
+   }
+]
+
+const contentWarningTags = [
+   {
+      "slug": "death",
+      "tag": "death"
+   },
+   {
+      "slug": "suicide-attempt",
+      "tag": "Suicide attempt"
+   },
+   {
+      "slug": "child-abuse",
+      "tag": "child abuse"
+   },
+   {
+      "slug": "fire-fire-injury",
+      "tag": "Fire/Fire injury"
+   }
+]
 export default function BookShow() {
    const params = useParams();
    const NONDIGIT_REGEX = /\D/g;
@@ -35,47 +125,80 @@ export default function BookShow() {
                      </svg>
                   </Close>
                   <div className="flex flex-col">
-                     <Close className="flex w-full">
-                        <Button variant="ghost" className="text-lg w-full justify-start">
-                           <BookmarkIcon className="mr-2" />
-                           Want to read
-                        </Button>
+                     <Close className="flex text-lg w-full justify-start hover:bg-gray-200 py-2 px-4 transition-colors">
+                        <BookmarkIcon className="mr-2" />
+                        Want to read
                      </Close>
-                     <Close className="flex w-full">
-                        <Button variant="ghost" className="text-lg w-full justify-start">
-                           <BookOpenTextIcon className="mr-2" />
-                           Currently Reading
-                        </Button>
+                     <Close className="flex text-lg w-full justify-start hover:bg-gray-200 py-2 px-4 transition-colors">
+                        <BookOpenTextIcon className="mr-2" />
+                        Currently Reading
                      </Close>
-                     <Close className="flex w-full">
-                        <Button variant="ghost" className="text-lg w-full justify-start">
-                           <BookCheckIcon className="mr-2" />
-                           Read
-                        </Button>
+                     <Close className="flex text-lg w-full justify-start hover:bg-gray-200 py-2 px-4 transition-colors">
+                        <BookCheckIcon className="mr-2" />
+                        Read
                      </Close>
+                     {/* TODO: Add more options */}
                   </div>
                </PopoverContent>
             </Popover>
             <StarRating initialRating={5} ratable onChange={() => { }} />
          </div>
          {/* book detail */}
-         <div className=" font-sans">
-            <h1 className="font-semibold  tracking-wide text-4xl">The Miracles of the Namiya General Store</h1>
-            <h2 className="text-lg">by Keigo Higashino</h2>
+         <div className=" font-sans ">
+            <h1 className="py-2 scroll-m-20 text-5xl font-semibold tracking-tight ">The Miracles of the Namiya General Store</h1>
+            <h2 className="text-lg pb-3">by Keigo Higashino</h2>
             <div className="flex items-center ">
                <StarRating initialRating={4.43} onChange={() => { }} />
                <span className="ml-3 text-2xl">4.43</span>
                <span className="ml-3 font-sans text-gray-300" >69 ratings</span>
                <span className="ml-3 font-sans text-gray-300" >68 reviews</span>
-
             </div>
-            <div>
-               <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, doloremque, similique eos consequuntur iure molestias natus esse optio sint earum cumque, officiis minus dolore placeat in voluptatibus repudiandae molestiae illo?
-
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, doloremque, similique eos consequuntur iure molestias natus esse optio sint earum cumque, officiis minus dolore placeat in voluptatibus repudiandae molestiae illo?
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, doloremque, similique eos consequuntur iure molestias natus esse optio sint earum cumque, officiis minus dolore placeat in voluptatibus repudiandae molestiae illo?
-               </p>
+            <ExpandableParagraph
+               text="Cuộc sống không phải là một đường thẳng tắp, cũng không phải là một bản sao mờ nhạt của người khác. Nếu bạn khao khát những điều phi thường, những thành tựu vượt bậc, những trải nghiệm độc đáo mà bạn chưa từng có, thì bạn phải sẵn sàng bước ra khỏi vùng an toàn của mình. Bạn phải dám làm những điều bạn chưa bao giờ làm, dám nghĩ những điều bạn chưa bao giờ nghĩ, dám thử những điều bạn chưa bao giờ thử. Tự duy ngược chính là chiếc la bàn dẫn đường cho bạn trên con đường khám phá tiềm năng vô hạn của bản thân và đạt được những điều tưởng chừng không thể."
+            />
+            <div className="grid grid-cols-3 gap-5 ">
+               <div>
+                  <h1 className="text-lg mb-2 font-semibold">
+                     Genres
+                  </h1>
+                  <div className="flex flex-wrap gap-2">
+                     {
+                        genreTags.map((item) => (
+                           <Link key={item.tag} to={"browse/tags/genre/" + item.slug} className="py-1 px-4 rounded-full bg-emerald-500">
+                              {item.tag}
+                           </Link>
+                        ))
+                     }
+                  </div>
+               </div>
+               <div>
+                  <h1 className="text-lg mb-2 font-semibold">
+                     Mood
+                  </h1>
+                  <div className="flex flex-wrap gap-2">
+                     {
+                        moodTags.map((item) => (
+                           <Link key={item.tag} to={"browse/tags/mood/" + item.slug} className="py-1 px-4 rounded-full bg-sky-500 first-letter:uppercase">
+                              {item.tag}
+                           </Link>
+                        ))
+                     }
+                  </div>
+               </div>
+               <div>
+                  <h1 className="text-lg mb-2 font-semibold">
+                     Content-warning
+                  </h1>
+                  <div className="flex flex-wrap gap-2">
+                     {
+                        contentWarningTags.map((item) => (
+                           <Link key={item.tag} to={"browse/tags/mood/" + item.slug} className="py-1 px-4 rounded-full bg-yellow-400 first-letter:uppercase text-black">
+                              {item.tag}
+                           </Link>
+                        ))
+                     }
+                  </div>
+               </div>
             </div>
          </div>
 
