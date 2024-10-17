7214da906df524286cb100f348d057a085153ce4
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-17 20:03:21 +0700

    chore: fix wrong env var name

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
 		<BrowserRouter>
 			<Auth0Provider
 				domain={import.meta.env.VITE_AUTH0_DOMAIN}
-				clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+				clientId={import.meta.env.VITE_AUTH0_CLIENTID}
 				authorizationParams={{
 					redirect_uri: window.location.origin,
 				}}
