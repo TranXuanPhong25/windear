34f5e6058f7a42bb0eb61225c8efc7961aa608fd
Rengumin <tranxuanphong26525@gmail.com>
2024-10-20 13:41:48 +0700

    Feat/auth (#4)
    * chore: tidy source
    
    * feat: add auth guarder
    
    * chore: intergrate from auth0 example
    
    * chore: integrate from auth0 example
    
    * feat: sample mock auth api
    
    * chore: remove scope authority
    
    * chore: remove logger from message.services
    
    * chore: remove supabase auth

--- src/components/auth/AuthenticationGuard.tsx
+++ src/components/auth/AuthenticationGuard.tsx
@@ -1,7 +1,7 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { ComponentType } from "react";
 
-export const AuthenticationGuard = ({ component}:{component:ComponentType}) => {
+export default function AuthenticationGuard({ component }: { component: ComponentType }) {
   const Component = withAuthenticationRequired(component, {
     onRedirecting: () => (
       <div className="page-layout">
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,13 +1,9 @@
 import BookList from "@/components/home/BookList";
 import NewsBanner from "@/components/home/NewsBanner";
-import { supabase } from "@/services/auth/supabaseAuth";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getProtectedResource, getPublicResource, getAdminResource } from "@/services/services/message.service";
 
 export default function HomePage() {
-
-
-
    const auth = useAuth0();
    return (
       <main className=" max-w-[1200px] mx-auto">
@@ -16,9 +12,7 @@ export default function HomePage() {
          <BookList title="Recommendations" brief />
          <BookList title="Popular" brief />
          <BookList title="New books" brief />
-         <div className="text-white space-x-3">
-            <button onClick={async () => console.log((await supabase.auth.getUser()).data)}>get</button>
-            <button onClick={async () => console.log((await auth.getIdTokenClaims()))}>getAuth0user</button>
+         <div className="text-white space-x-3">={async () => console.log((await auth.getIdTokenClaims()))}>getAuth0user</button>
             <button onClick={async () => console.log(await getProtectedResource(await auth.getAccessTokenSilently()))}>getProtectedResource</button>
             <button onClick={async () => console.log((await getAdminResource(await auth.getAccessTokenSilently())).data)}>getAdminResource</button>
             <button onClick={async () => console.log(await getPublicResource())}>getPublicResource</button>
