da618f488bbd7431ffa8965d9f12324becc47100
= <=>
2024-10-31 10:43:59 +0700

    fix: reduce image processing

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import {
   motion,
@@ -103,8 +102,8 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white ">
-        The Wonderful <br /> Library on the world
+      <h1 className="text-4xl md:text-8xl font-bold dark:text-white ">
+        The wonderful <br /> Library on the world
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
@@ -135,24 +134,22 @@ export const ProductCard = ({
         y: -20,
       }}
       key={product.title}
-      className="group/product h-96 w-[30rem] relative flex-shrink-0 rounded-lg overflow-hidden "
+      className="group/product h-[340px] w-[30rem] relative flex-shrink-0 rounded-2xl overflow-hidden"
     >
       <Link
         to={product.link}
-        className="block group-hover/product:shadow-2xl "
-      >
-        <img
-          src={product.thumbnail}
-          height="600"
-          width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0"
-          alt={product.title}
-        />
+        className="block group-hover/product:shadow-2xl w-full bg-gray-400 absolute ">
+          <img
+            src={product.thumbnail}
+            className="object-cover object-left-top absolute inset-0"
+            alt={product.title}
+          />
+
       </Link>
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
+      <div className="relative text-center opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
-      </h2>
+      </div>
     </motion.div>
   );
 };
