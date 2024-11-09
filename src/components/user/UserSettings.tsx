9600305422c4dc6776f7cf0380a2db579bedfcf0
TranXuanPhong25 <tranxuanphong26525@gmail.com>
2024-11-09 19:35:32 +0700

    chore: change input backgorund

--- src/components/user/UserSettings.tsx
+++ src/components/user/UserSettings.tsx
@@ -1,7 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import ProfileInput from "../layout/user/settings/ProfileInput";
 import { Separator } from "../ui/separator";
 
 function ProfileSettings() {
+   const {user} = useAuth0();
    return (
       <section id="user-profile ">
          <h1 className="text-3xl">Profile Settings</h1>
@@ -11,10 +13,10 @@ function ProfileSettings() {
             <div className="w-full mr-12 ">
                <ProfileInput />
             </div>
-            <div className="w-48 mr-24 ">
+            <div className="w-48 mr-24 text-center">
                <h1>Profile picture</h1>
                <div className="overflow-hidden rounded-full size-48 mt-2">
-                  <img src="https://via.placeholder.com/150" alt="profile"
+                  <img src={user?.picture} alt="profile"
                      className="w-full" />
                </div>
             </div>
