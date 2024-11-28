a4f1e6178abb88c06a4d9f14756c1a4c0d967940
TranXuanPhong25 <tranxuanphong26525@gmail.com>
2024-11-28 07:33:57 +0700

    feat: shelves responsive

--- src/pages/user/Shelves.tsx
+++ src/pages/user/Shelves.tsx
@@ -13,8 +13,26 @@ import {Separator} from "@/components/ui/separator.tsx";
 
 const readingList = ["Want to read", "Currently reading", "Read"];
 export default function Shelves() {
+    const targetRef = React.useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const ref = targetRef.current;
+        const handleResize = () => {
+            if (ref) {
+                ref.style.height = 'auto';
+                ref.style.height = ref.scrollHeight + 'px';
+            }
+        }
+        document.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            document.removeEventListener('resize', handleResize);
+
+
+        }
+    }, []);
     useEffect(() => {
         document.title = "Shelves - Windear library";
+
     }, []);
     const {
         data: {
@@ -40,9 +58,9 @@ export default function Shelves() {
         }
     }
     return (
-        <div className="flex dark:text-white pr-2 max-w-5xl mx-auto min-h-[90vh]">
+        <div className="flex flex-col lg:flex-row dark:text-white pr-2 max-w-5xl mx-auto min-h-[90vh]">
 
-            <div className="w-[260px] ml-10 sticky h-fit top-24">
+            <div className="w-full lg:w-[260px]  lg:sticky h-fit top-24z">
                 <Accordion type="multiple" defaultValue={["item-1"]} className=" ">
                     <AccordionItem value="item-1" className=" border-b-0 w-full max-w-[900px]  ">
                         <AccordionTrigger className="py-2 text-xl">
@@ -96,26 +114,33 @@ export default function Shelves() {
                 </Accordion>
 
             </div>
-            <div className="flex flex-wrap w-full ml-6">
+            <div className="flex flex-wrap w-full lg:ml-6">
                 {
                     displayedShelves.map((shelf) => (
                         <div key={shelf.name} className="w-full ">
                             <h2 className="text-2xl ">{shelf.name}</h2>
-                            <div className="flex flex-wrap ml-6 mt-6">
+                            <div className="flex flex-wrap md:ml-6 mt-6">
                                 {
                                     shelf.books.length === 0 ? (
                                         <p className="text-lg dark:text-gray-300">No books in this shelf yet</p>
                                     ) : (
                                         isLoadingShelvesData ? <LoadingBlock/> : shelf.books.map((book) => (
-                                            <div key={book.id}
-                                                 className="flex w-full mt-10 mb-10 border-b-2 dark:border-gray-300/20 border-gray-400/50 relative items-center dark:bg-slate-600 rounded-xl bg-zinc-200/30   ">
-                                                <img src={book.imageUrl || "/book-cover-unavailable-placeholder.jpg"}
-                                                     alt={book.title}
-                                                     className="object-fit w-[200px] rounded-r-xl rounded-l-sm  -mt-8 -mb-2  ml-4 shadow-[0px_7px_20px_0px_rgba(10,10,10,0.4)] "/>
-                                                <div className="  w-full h-[240px]  pl-2 pr-4 py-1 ml-3 rounded-lg ">
-                                                    <div className='flex flex-col justify-between h-full'>
-                                                        <div>
-                                                            <Link to={"/books/" + book.id} className="text-3xl pr-4">{book.title}</Link>
+                                            <div key={book.id + shelf.name}
+                                                 className="flex  w-full md:my-10 mt-6 border-b-2 dark:border-gray-300/20 border-gray-400/50 relative items-center dark:bg-slate-600 rounded-xl bg-zinc-200/30   ">
+                                                <Link to={"/books/" + book.id}
+                                                >
+                                                    <img
+                                                        src={book.imageUrl || "/book-cover-unavailable-placeholder.jpg"}
+                                                        alt={book.title}
+                                                        className="min-[525px]:block hidden object-fit w-[100px] md:w-[200px] rounded-r-xl rounded-l-sm  md:-mt-8 -mb-2 ml-2 md:ml-4 shadow-[0px_7px_20px_0px_rgba(10,10,10,0.4)] "/>
+                                                </Link>
+                                                <div className="  w-full md:h-[240px]  pl-2 pr-4 py-1 min-[535px]:ml-6  rounded-lg ">
+                                                    <div className=' flex flex-col-reverse  min-[535px]:flex-col  justify-between h-full'>
+
+                                                        <div className=" w-full lg:max-w-[424px]  xl:max-w-[434px]"
+                                                             ref={targetRef}>
+                                                            <Link to={"/books/" + book.id}
+                                                                  className=" text-2xl lg:text-3xl break-words md:pr-5 ">{book.title}</Link>
                                                             <h4><span
                                                                 className='dark:text-gray-200'>by</span> {book.author} - {new Date(book.releaseDate).getFullYear()}
                                                             </h4>
@@ -126,20 +151,31 @@ export default function Shelves() {
                                                                     className='fill-yellow-400 text-transparent ml-1'/>
                                                             </div>
                                                         </div>
-                                                        <div className='flex justify-between items-center'>
 
-                                                            <StarRating bookId={book.id}
-                                                                        ratable
-                                                                        onChange={() => {
-                                                                        }}
+                                                        <div className='flex justify-between h-fit mb-8 min-[535px]:block min-[535px]:mb-0'>
+                                                            <Link to={"/books/" + book.id} className="min-[535px]:hidden block"
+                                                            >
+                                                                <img
+                                                                    src={book.imageUrl || "/book-cover-unavailable-placeholder.jpg"}
+                                                                    alt={book.title}
+                                                                    className=" object-fit w-[100px] md:w-[200px] rounded-r-xl rounded-l-sm  md:-mt-8 -mb-2 ml-2 md:ml-4 shadow-[0px_7px_20px_0px_rgba(10,10,10,0.4)] "/>
+                                                            </Link>
+                                                           <div className='flex justify-between items-center min-[535px]:flex-row flex-col-reverse'>
+                                                               <StarRating bookId={book.id}
+                                                                           ratable
+                                                                           onChange={() => {
+                                                                           }}
 
-                                                            />
-                                                            <div>
-                                                                <h4>AddedDate: {new Date(book.addedDate).toLocaleDateString()}</h4>
-                                                                <ShelfAction customClass="!mt-1 w-full " book={book}/>
-                                                                {/* <GetBook /> */}
-                                                            </div>
+                                                               />
+                                                               <div>
+                                                                   <h5>AddedDate: {new Date(book.addedDate).toLocaleDateString()}</h5>
+                                                                   <ShelfAction customClass="!mt-1 w-full " book={book}/>
+                                                                   {/* <GetBook /> */}
+                                                               </div>
+                                                           </div>
+
                                                         </div>
+
                                                     </div>
                                                     <div>
 
@@ -147,7 +183,7 @@ export default function Shelves() {
                                                 </div>
                                                 <div>
                                                     <Link to={"/books/" + book.id}
-                                                          className="absolute top-6 right-4 !bg-transparent"><SquareArrowOutUpRight/></Link>
+                                                          className="absolute top-6 right-4 !bg-transparent md:block hidden"><SquareArrowOutUpRight/></Link>
                                                 </div>
                                             </div>
                                         ))
@@ -156,7 +192,7 @@ export default function Shelves() {
                                 }
                             </div>
                             {
-                                displayedShelves.length !==1 && <Separator className="my-3"/>
+                                displayedShelves.length !== 1 && <Separator className="my-3"/>
                             }
                         </div>
                     ))
